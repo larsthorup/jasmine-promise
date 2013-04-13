34fadfe726f3571e58251b1398342de1c041775d
Lars Thorup <lars@zealake.com>
2013-04-13 10:53:58 -0700

    slowness added

--- src/js/page.js
+++ src/js/page.js
@@ -6,17 +6,19 @@
     };
     Page.prototype.load = function (mustSucceed) {
         var def = $.Deferred();
-        // ToDo: slowness
         if (mustSucceed) {
-            def.resolve('loaded');
+            window.setTimeout(function () {
+                def.resolve('loaded');
+            }, 600);
         }
         return def.promise();
     };
     Page.prototype.click = function (mustSucceed) {
         var def = $.Deferred();
-        // ToDo: slowness
         if (mustSucceed) {
-            def.resolve('clicked');
+            window.setTimeout(function () {
+                def.resolve('clicked');
+            }, 300);
         }
         return def.promise();
     };
